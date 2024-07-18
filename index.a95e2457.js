ae1db7856295e6807a66ecd12ad178c65f2d9270
Bohdan Ivankov <bodanich600@gmail.com>
2024-07-18 07:51:30 +0100

    make build

---
+++ index.a95e2457.js
@@ -0,0 +1,2 @@
+Array.from(document.querySelector("table").rows).forEach(function(e){var l=e.cells[1].cloneNode(!0);e.insertBefore(l,e.cells[e.cells.length-1])});
+//# sourceMappingURL=index.a95e2457.js.map
